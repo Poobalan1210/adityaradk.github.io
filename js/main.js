cffcb95c28cbbb582ee8f6d3cd4c28e600c011aa
Aditya Radhakrishnan <aditya.radhakrishnanbot@gmail.com>
2017-10-19 09:39:17 +0530

    added scroll effect

--- js/main.js
+++ js/main.js
@@ -14,3 +14,10 @@ $('.pushpin-demo-nav').each(function() {
     bottom: $target.offset().top + $target.outerHeight() - $this.height()
   });
 });
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
