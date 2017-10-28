c7eae8e5ac6ba51efa53c70e2437e87dc7d39333
Aditya Radhakrishnan <aditya.radhakrishnanbot@gmail.com>
2017-10-28 11:45:47 +0530

    Added slider JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+  $('.carousel').carousel();
+  // Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+
+// Destroy carousel
+$('.carousel').carousel('destroy');
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+});
+
+
+
+$(document).ready(function(){
   $('.target').pushpin({
     top: 0,
     bottom: 1000,
